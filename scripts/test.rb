8b0168ac9f249c4d80ed8458d0ae4523670db03b
Jakob Schwendner <jakob.schwendner@dfki.de>
2013-07-10 15:21:57 +0200

    fixed test gripper configuration and added test script

---
+++ scripts/test.rb
@@ -0,0 +1,40 @@
+#! /usr/bin/ruby
+
+require 'orocos'
+include Orocos
+
+if !ARGV[0]
+    STDERR.puts "usage: test.rb <device name>"
+    exit 1
+end
+
+Orocos.initialize
+
+    Orocos::Process.run 'servo_dynamixel::Task' => 'dynamixel_task', 'output' => nil do |p|
+    driver = p.task 'dynamixel_task'
+
+    driver.device = ARGV[0]
+    driver.baudrate = 125000
+
+    Orocos.apply_conf_file(driver, 'dynamixel.yml', ['default'])
+
+    driver.configure
+    driver.start
+
+    writer = driver.command.writer
+
+    # wait for input
+    puts "enter command: (name, position)"
+    while not (cmd = $stdin.readline.chomp).empty? do
+	name, position = cmd.split(",")
+	position = position.to_f
+
+	joints = Types::Base::Samples::Joints.new
+	joints.names << name
+	joint_state = Types::Base::JointState.new
+	joint_state.position = position
+	joints.elements << joint_state
+	writer.write joints
+    end
+end
+
